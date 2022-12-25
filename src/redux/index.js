7f79e484ae618ca2ca48465777b1b88db3865271
Dwibi <andikabizar2004@gmail.com>
2022-12-26 01:50:25 +0700

    REAL FINAL WEB

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,6 +1,5 @@
 import { legacy_createStore as createStore } from "redux";
 import { applyMiddleware } from "redux";
-import { createLogger } from "redux-logger";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
 
